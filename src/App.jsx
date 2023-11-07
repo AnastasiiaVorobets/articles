639dddae6bfe9aac01aa77611fa044e8d7e8ffa7
AnastasiiaVorobets <anastasiiavorobets03@gmal.com>
2023-11-07 15:58:12 +0200

    fixed route

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<Home />} />
         <Route path="/news" element={<News />} />
       </Routes>
   </Router>
